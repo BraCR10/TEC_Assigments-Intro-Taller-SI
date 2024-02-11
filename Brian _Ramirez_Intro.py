5e420d475cb607e2071fb64764f17971a3e97fcc
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-02-10 19:10:10 -0600

    Primeras tareas

---
+++ Brian _Ramirez_Intro.py
@@ -0,0 +1,13 @@
+
+x=2
+y=4
+z=1
+
+print('El resultado de la primera operacion es:',( x**3 + y**5 + z**2 )**2 * (x*y*z))
+# El resultado es 8536712
+
+x=4
+y=3
+z=3
+print('El resultado de la segunda operacion es:',( (x+y+z)**(x*y*z)  * (x**3*z**2*z**3))**2)
+# El resultado es 241864704000000000000000000000000000000000000000000000000000000000000000000000000
