84612c3ffc486d43b9cb7884717fabaf9b20dc72
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-02-22 11:16:42 -0600

    Clases 22_2_24

--- Nums de izq a dre .py
+++ Nums de izq a dre .py
@@ -12,6 +12,12 @@ def lenNum(num):#Funcion del largo
             return cont
     else:
         return 'Deben ser enteros'
+#Menejar numero de izq a der
+num=3056#Este metodo no toma los 0s, ¡Cuidado!
+print('Numero original:', num,' \n')
+while num!=0:
+    temp1=num//(10**(lenNum(num)-1))#Extrae el numero
+    print('Primer numero de izq a der: ',temp1)
+    num%=(10**(lenNum(num)-1))#Crea el nuevo numero
+    print('El numero que va quedando',num,' \n')
 
-
-print(lenNum(-12))
